c3a8305323eec2d859af066d683f0e536fd23933
Bhasker Yadav <bhasker.yadav@accenture.com>
2016-04-06 12:12:16 +0530

    Initial structure

---
+++ SharedLibrary/src/main/java/com/afby/library/SharedLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.afby.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedLibraryApplication.class, args);
+	}
+}
---
+++ SharedLibrary/src/test/java/com/afby/library/SharedLibraryApplicationTests.java
@@ -0,0 +1,18 @@
+package com.afby.library;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SharedLibraryApplication.class)
+@WebAppConfiguration
+public class SharedLibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
